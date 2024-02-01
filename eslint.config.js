a2992f14ed78ff0908d721e6f512e031be101b67
Brendan Dash <me@aiwan.run>
2024-02-01 11:55:52 +0800

    feat: MVP

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+// @ts-check
+const config = require("@debbl/eslint-config").default;
+
+module.exports = config({
+  typescript: true,
+});
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import vscode from "vscode";
+
+const MAP: Record<string, string> = {
+  true: "false",
+  false: "true",
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand("toggleBoolean", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    const { document } = editor;
+
+    const wordRange = document.getWordRangeAtPosition(editor.selection.active);
+    if (!wordRange) return;
+
+    const word = document.getText(wordRange);
+    const replaceWord = MAP[word];
+    if (!replaceWord) return;
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(wordRange, replaceWord);
+    });
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
